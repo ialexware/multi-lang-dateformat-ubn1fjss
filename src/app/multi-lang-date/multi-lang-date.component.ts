9e789e14622afd30b076e2d6d4c5b94397069e91
ialexware <edwinglzmtz@outlook.com>
2023-07-27 19:54:46 -0600

    5 files modified

--- src/app/multi-lang-date/multi-lang-date.component.ts
+++ src/app/multi-lang-date/multi-lang-date.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
-import {TranslateService} from '@ngx-translate/core';
-import {DateAdapter} from '@angular/material';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { DateAdapter } from '@angular/material';
 import {
   ControlValueAccessor,
   FormControl,
@@ -22,15 +22,70 @@ export enum Languages {
 @Component({
   selector: 'app-multi-lang-date',
   templateUrl: './multi-lang-date.component.html',
-  styleUrls: ['./multi-lang-date.component.css']
+  styleUrls: ['./multi-lang-date.component.css'],
 })
-export class MultiLangDateComponent implements OnInit{
+export class MultiLangDateComponent implements OnInit {
   public selectedDate: Date = new Date();
-  public dateControl = new FormControl(this.selectedDate, [Validators.required]);
+  public dateControl = new FormControl(this.selectedDate, [
+    Validators.required,
+  ]);
 
   public languageList: string[] = [];
+  /**
+   * Getter and setter for the minimum date which is available for selection.
+   */
+  @Input() get minDate(): Date | undefined {
+    return this.minLocalDate!;
+  }
+  /**
+   * Setter for the minimum date which is available for selection.
+   */
+  set minDate(value: Date | undefined) {
+    if (value) {
+      value?.setHours(0, 0, 0, 0);
+      if (value !== this.minLocalDate) {
+        this.minLocalDate = value;
+        this.dateControl.updateValueAndValidity();
+      }
+    }
+  }
 
-  constructor(private translate: TranslateService, private dateAdapter: DateAdapter<Date>) {}
+  /**
+   * Getter and setter for the maximum date which is available for selection.
+   */
+  @Input() get maxDate(): Date | undefined {
+    return this.maxLocalDate!;
+  }
+
+  /**
+   * Setter for the maximum date which is available for selection.
+   */
+  set maxDate(value: Date | undefined) {
+    if (value) {
+      value?.setHours(0, 0, 0, 0);
+      if (value !== this.maxLocalDate) {
+        this.maxLocalDate = value;
+        this.dateControl.updateValueAndValidity();
+      }
+    }
+  }
+  /**
+   * Min date to ensure that the date provided to 'ngx-bootstrap/datepicker' has no hours.
+   */
+  public minLocalDate?: Date;
+
+  /**
+   * Max date to ensure that the date provided to 'ngx-bootstrap/datepicker' has no hours.
+   */
+  public maxLocalDate?: Date;
+
+  public placeholder = '';
+  constructor(
+    private translate: TranslateService,
+    private dateAdapter: DateAdapter<Date>
+  ) {
+    this.placeholder = this.getDateFormat()
+  }
 
   ngOnInit() {
     this.languageList = Object.keys(Languages).filter(String);
@@ -39,5 +94,38 @@ export class MultiLangDateComponent implements OnInit{
   useLanguage(language: Languages): void {
     this.translate.use(language);
     this.dateAdapter.setLocale(language);
+  }
+
+  /**
+   * Return the date formatted
+   * @param {String} date Date to format
+   * @returns A string with the date formatted according to the locale
+   */
+  public formatDate(date: Date): string {
+    return Intl.DateTimeFormat(navigator.language).format(date);
+  }
+
+
+    /**
+     * Get the date format according to the user configuration language using the Intl API
+     * as the MatNativeDateModule does.
+     * @returns {string} Returns the date format according to the user configuration language
+     */
+     getDateFormat() {
+      const formatter = new Intl.DateTimeFormat(this.language).formatToParts();
+      return formatter
+          .map((e) => {
+              switch (e.type) {
+                  case 'month':
+                      return 'mm';
+                  case 'day':
+                      return 'dd';
+                  case 'year':
+                      return 'yyyy';
+                  default:
+                      return e.value;
+              }
+          })
+          .join('');
   }
 }
--- src/app/shared/date-adapter.ts
+++ src/app/shared/date-adapter.ts
@@ -1,5 +1,8 @@
-import {NativeDateAdapter} from '@angular/material';
-import {DatePipe} from '@angular/common';
+import { Platform } from '@angular/cdk/platform';
+import { DatePipe } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { MAT_DATE_LOCALE, NativeDateAdapter } from '@angular/material/core';
+import { TranslateService } from '@ngx-translate/core';
 
 export interface DateDisplay {
   year: string;
@@ -21,27 +24,50 @@ display: {
 };
 
 export class CustomDatePickerAdapter extends NativeDateAdapter {
-  parse(value: string | number): Date | null {
-    if ((typeof value === 'string') && (value.indexOf('.') > -1)) {
-      const str: string[] = value.split('.');
-      if (str.length < 2 || isNaN(+str[0]) || isNaN(+str[1]) || isNaN(+str[2])) {
-        return null;
-      }
-      return  new Date(Number(str[2]), Number(str[1]) - 1, Number(str[0]));
-    }
-    const timestamp: number = typeof value === 'number' ? value : Date.parse(value);
-    return isNaN(timestamp) ? null : new Date(timestamp);
-  }
 
-  format(date: Date, display: string | DateDisplay): string {
-    if (display === 'customInput') {
-      console.log('customInput')
-      console.log(new DatePipe(this.locale).transform(date, 'shortDate'))
-      return new DatePipe(this.locale).transform(date, 'shortDate');
-    } else {
-      console.log('no')
-      console.log(new DatePipe(this.locale).transform(date, 'MMM yyyy'))
-      return  new DatePipe(this.locale).transform(date, 'MMM yyyy');
+    override parse(value: string | number): Date | null {
+        if (typeof value === 'string') {
+            const formatter: any[] = new Intl.DateTimeFormat(this.locale).formatToParts();
+            const delimiter = formatter.find((e) => e.type === 'literal').value;
+            const format = this.getDateFormat();
+            const newDate = this.stringToDate(value, format, delimiter);
+            console.log(newDate);
+            return newDate;
+        }
+        const timestamp: number = typeof value === 'number' ? value : Date.parse(value);
+        console.log('value', value);
+        console.log('timestamp', timestamp);
+        return isNaN(timestamp) ? null : new Date(timestamp);
     }
-  }
+
+    private getDateFormat(date: Date = new Date()) {
+        const formatter: any[] = new Intl.DateTimeFormat(this.locale).formatToParts(date);
+        console.log(formatter);
+        return formatter
+            .map((e) => {
+                switch (e.type) {
+                    case 'month':
+                        return 'MM';
+                    case 'day':
+                        return 'dd';
+                    case 'year':
+                        return 'yyyy';
+                    default:
+                        return e.value;
+                }
+            })
+            .join('');
+    }
+    private stringToDate(_date: string, _format: string, _delimiter: string) {
+        const formatLowerCase = _format.toLowerCase();
+        const formatItems = formatLowerCase.split(_delimiter);
+        const dateItems = _date.split(_delimiter);
+        const monthIndex = formatItems.indexOf('mm');
+        const dayIndex = formatItems.indexOf('dd');
+        const yearIndex = formatItems.indexOf('yyyy');
+        let month = parseInt(dateItems[monthIndex], 10);
+        month -= 1;
+        const formatedDate = new Date(+dateItems[yearIndex], month, +dateItems[dayIndex]);
+        return formatedDate;
+    }
 }
